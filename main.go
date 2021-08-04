34e4ae7b875bf6d9ed0a97baf1e67d70512cc1b2
Dakota Walsh <kota@nilsu.org>
2021-08-04 14:51:11 +1200

    initial commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	gemtext "git.sr.ht/~kota/goldmark-gemtext"
+	"git.sr.ht/~sircmpwn/getopt"
+)
+
+var (
+	in  = os.Stdin
+	out = os.Stdout
+)
+
+func usage() {
+	log.Fatal("Usage: gemgen [-i] [-o]")
+}
+
+func main() {
+	log.SetPrefix("")
+	log.SetFlags(0)
+	opts, _, err := getopt.Getopts(os.Args, "ei:o:")
+	if err != nil {
+		log.Print(err)
+		usage()
+	}
+	for _, opt := range opts {
+		switch opt.Option {
+		case 'e':
+			gemtext.Emphasis = true
+		case 'i':
+			if opt.Value == "-" {
+				continue
+			}
+			in, err = os.Open(opt.Value)
+			if err != nil {
+				log.Print(err)
+				usage()
+			}
+		case 'o':
+			out, err = os.Create(opt.Value)
+			if err != nil {
+				log.Print(err)
+				usage()
+			}
+		}
+	}
+
+	// load markdown
+	source, err := io.ReadAll(in)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse markdown and write reformatted source
+	err = gemtext.Format(source, out)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
