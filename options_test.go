2eb290896886bcf3340179c3f28f38c6f3d19d11
Dakota Walsh <kota@nilsu.org>
2021-09-23 18:13:42 +1200

    add HeadingLinkBelow test case

--- options_test.go
+++ options_test.go
@@ -30,6 +30,10 @@ func TestOptions(t *testing.T) {
 			[]gem.Option{gem.WithHeadingLink(gem.HeadingLinkOff)},
 		},
 		{
+			[]string{"-a", "below"},
+			[]gem.Option{gem.WithHeadingLink(gem.HeadingLinkBelow)},
+		},
+		{
 			[]string{"-p", "off"},
 			[]gem.Option{gem.WithParagraphLink(gem.ParagraphLinkOff)},
 		},
