b51121aa1dc075dc6a1341988eb9f21fc995a387
Dakota Walsh <kota@nilsu.org>
2021-09-30 21:23:38 +1300

    improve comment style

--- convert.go
+++ convert.go
@@ -16,7 +16,7 @@ import (
 
 // convert reads markdown data and writes it as gemtext using opts.
 func convert(r io.Reader, w io.Writer, opts []gem.Option) error {
-	// create markdown parser
+	// Create markdown parser.
 	var buf bytes.Buffer
 	md := goldmark.New(
 		goldmark.WithExtensions(
@@ -24,13 +24,14 @@ func convert(r io.Reader, w io.Writer, opts []gem.Option) error {
 			extension.Strikethrough,
 		),
 	)
-	// read markdown
+
+	// Read markdown.
 	src, err := io.ReadAll(r)
 	if err != nil {
 		return fmt.Errorf("failed to read input file: %v", err)
 	}
 
-	// render
+	// Render to gemtext.
 	md.SetRenderer(gem.New(opts...))
 	if err := md.Convert(src, &buf); err != nil {
 		return fmt.Errorf("failed to convert markdown to gemtext: %v", err)
@@ -43,33 +44,37 @@ func convert(r io.Reader, w io.Writer, opts []gem.Option) error {
 // Files are written with the .gmi extension in the source directory.
 // Encountering an error stops the program with an appropriate message.
 func convertFiles(opts *Opts) {
-	// read and convert the list of files concurrently
+	// Read and convert the list of files concurrently.
 	var wg sync.WaitGroup
 	for _, name := range opts.Names {
 		wg.Add(1)
 		go func(name string) {
-			// decrement the counter when the goroutine completes
+			// Decrement the counter when the goroutine completes.
 			defer wg.Done()
-			// read input file
+
+			// Read input file.
 			src, err := os.Open(name)
 			if err != nil {
 				log.Fatalf("failed reading file %s: %v\n", name, err)
 			}
-			// open output file
+
+			// Open output file.
 			base := filepath.Base(name)
 			outName := base[0:len(base)-len(filepath.Ext(base))] + ".gmi"
 			var outPath string
 			if opts.Output != "" {
-				// write to specified output directory
+				// Use input directory as output directory.
 				outPath = filepath.Join(opts.Output, outName)
 			} else {
-				// write to source directory
+				// Use configured output directory.
 				outPath = filepath.Join(filepath.Dir(name), outName)
 			}
 			out, err := os.OpenFile(outPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 			if err != nil {
 				log.Fatalf("failed opening output file %s: %v\n", outPath, err)
 			}
+
+			// Convert to gemtext and write output.
 			err = convert(src, out, opts.GemOptions)
 			if err != nil {
 				log.Fatalf("failed converting file %s: %v\n", name, err)
