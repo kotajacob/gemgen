6d718830585613dfa8ea92ab0b2091ccbbae4b84
Dakota Walsh <kota@nilsu.org>
2021-10-01 14:46:25 +1300

    reorganize convert.go

--- convert.go
+++ convert.go
@@ -14,6 +14,52 @@ import (
 	"github.com/yuin/goldmark/extension"
 )
 
+// convertFiles reads Opts and converts the list of named files concurrently.
+// Files are written with the .gmi extension in the source directory.
+// Encountering an error stops the program with an appropriate message.
+func convertFiles(opts *Opts) error {
+	// Read and convert the list of files concurrently.
+	var wg sync.WaitGroup
+	for _, name := range opts.Names {
+		wg.Add(1)
+		go func(name string) error {
+			// Decrement the counter when the goroutine completes.
+			defer wg.Done()
+
+			// Read input file.
+			src, err := os.Open(name)
+			if err != nil {
+				log.Fatalf("failed reading input file %s: %v\n", name, err)
+			}
+
+			// Open output file.
+			base := filepath.Base(name)
+			outName := base[0:len(base)-len(filepath.Ext(base))] + ".gmi"
+			var outPath string
+			if opts.Output != "" {
+				// Use input directory as output directory.
+				outPath = filepath.Join(opts.Output, outName)
+			} else {
+				// Use configured output directory.
+				outPath = filepath.Join(filepath.Dir(name), outName)
+			}
+			out, err := os.OpenFile(outPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+			if err != nil {
+				log.Fatalf("failed opening output file %s: %v\n", outPath, err)
+			}
+
+			// Convert to gemtext and write output.
+			err = convert(src, out, opts.GemOptions)
+			if err != nil {
+				log.Fatalf("failed converting file %s: %v\n", name, err)
+			}
+			return nil
+		}(name)
+	}
+	wg.Wait()
+	return nil
+}
+
 // convert reads markdown data and writes it as gemtext using opts.
 func convert(r io.Reader, w io.Writer, opts []gem.Option) error {
 	// Create markdown parser.
@@ -38,48 +84,4 @@ func convert(r io.Reader, w io.Writer, opts []gem.Option) error {
 	}
 	io.Copy(w, &buf)
 	return nil
-}
-
-// convertFiles reads Opts and converts the list of named files concurrently.
-// Files are written with the .gmi extension in the source directory.
-// Encountering an error stops the program with an appropriate message.
-func convertFiles(opts *Opts) {
-	// Read and convert the list of files concurrently.
-	var wg sync.WaitGroup
-	for _, name := range opts.Names {
-		wg.Add(1)
-		go func(name string) {
-			// Decrement the counter when the goroutine completes.
-			defer wg.Done()
-
-			// Read input file.
-			src, err := os.Open(name)
-			if err != nil {
-				log.Fatalf("failed reading file %s: %v\n", name, err)
-			}
-
-			// Open output file.
-			base := filepath.Base(name)
-			outName := base[0:len(base)-len(filepath.Ext(base))] + ".gmi"
-			var outPath string
-			if opts.Output != "" {
-				// Use input directory as output directory.
-				outPath = filepath.Join(opts.Output, outName)
-			} else {
-				// Use configured output directory.
-				outPath = filepath.Join(filepath.Dir(name), outName)
-			}
-			out, err := os.OpenFile(outPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-			if err != nil {
-				log.Fatalf("failed opening output file %s: %v\n", outPath, err)
-			}
-
-			// Convert to gemtext and write output.
-			err = convert(src, out, opts.GemOptions)
-			if err != nil {
-				log.Fatalf("failed converting file %s: %v\n", name, err)
-			}
-		}(name)
-	}
-	wg.Wait()
 }
